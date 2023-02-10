ec75eb239caf63536f4019725ac7493062c3bbba
achmad rafi raichansyah <110074043+AchmadRafiR@users.noreply.github.com>
2023-02-10 21:39:26 +0700

    Add files via upload

--- tugas-DummyJSON/js/app.js
+++ tugas-DummyJSON/js/app.js
@@ -8,7 +8,7 @@ function getData() {
         data: "contentType",
         dataType: "json",
         success: function (response) {
-            console.log(response);
+            // console.log(response);
             
             out += `<tr>
             <th>Title</th>
@@ -42,7 +42,7 @@ function showData() {
         data: "contentType",
         dataType: "json",
         success: function (response) {
-            console.log(response)
+            // console.log(response)
 
             $.each(response, function (key, val) { 
                 out += `<button type="button" class="btn btn-dark m-1" id="filter" value='${val}'>${val}</button>`;
@@ -62,7 +62,7 @@ function filterData(cat) {
         data: "contentType",
         dataType: "json",
         success: function (response) {
-            console.log(response)
+            // console.log(response)
 
             $.each(response.products, function (key, val) { 
                 out += `<tr>
@@ -115,28 +115,37 @@ function addData() {
         title: title,
         description: description,
         category: category,
-    };    
-    fetch(baseUrl + "/products/add", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-        title: data["title"],
-        description: data["description"],
-        category: data["category"]
-        /* other product data */
-        })
-    })
-    .then(res => res.json())
-    .then(console.log(data))
-    .then(alert(data["title"] +  " Berhasil diTambahkan"))
+    };
+    $.ajax({
+        type: "post",
+        url: baseUrl + "/products/add",
+        contentType: 'application/json',
+        dataType: "json",
+        data: JSON.stringify({
+            title: data["title"],
+            description: data["description"],
+            category: data["category"]
+            }),
+        success: function (response) {
+            console.log(response);
+            console.log(data);
+            alert(data["title" ] + "insert");
+        }
+    });
 }
 
 $("#save").click(function (e) { 
     e.preventDefault();
+    id = $("#id").val();
     title = $("#title").val();
     description = $("#des").val();
     category = $("#cat").val();
 
+    // if (id != null) {
+    //     updateData(id);
+    // } else {
+    //     addData();
+    // }
     if (id != null) {
         addData();
     } else {
@@ -170,19 +179,35 @@ function updateData(id) {
         title: title,
         description: description,
         category: category,
-    }
-    fetch(baseUrl + "/products/" + id, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+    };
+    $.ajax({
+        type: "PATCH",
+        url: baseUrl + "/products/" + id,
+        contentType: "application/json",
+        data: "json",
+        dataType: JSON.stringify({
             title: data["title"],
             description: data["description"],
             category: data["category"]
-        })
-    })
-    .then(res => res.json())
-    .then(console.log)
-    .then(alert(data["title"] + " Berhasil diUbah"))
+            }),
+        success: function (response) {
+            // console.log(response);
+            console.log(data);
+            alert(data["title" ] + " update");
+        }
+    });
+    // fetch(baseUrl + "/products/" + id, {
+    //     method: 'PATCH',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //         title: data["title"],
+    //         description: data["description"],
+    //         category: data["category"]
+    //     })
+    // })
+    // .then(res => res.json())
+    // .then(console.log)
+    // .then(alert(data["title"] + " Berhasil diUbah"))
 
 }
 
@@ -214,3 +239,172 @@ $("#delete").click(function (e) {
 });
 
 getData()
+
+
+
+
+// DATA PELANGGAN
+
+$(document).ready(function () {
+    let id ="";
+    let pelanggan ="";
+    let alamat = "";
+    let telp ="";
+
+    $("#submit").click(function (e) { 
+        e.preventDefault();
+        id = $("#id").val();
+        pelanggan = $("#pelanggan").val();
+        alamat = $("#alamat").val();
+        telp = $("#telp").val();
+
+        if (id == "") {
+            insertData();
+        } else {
+            updateData();
+        }
+
+        $("#id").val("");
+        $("#pelanggan").val("");
+        $("#alamat").val("");
+        $("#telp").val("");
+    });
+
+    $("#btn-tambah").click(function (e) { 
+        e.preventDefault();
+
+        $("#title").html("Tambah Data");
+
+        $("#id").val("");
+        $("#pelanggan").val("");
+        $("#alamat").val("");
+        $("#telp").val("");
+
+    });
+
+    $("tbody").on("click",".btn-del", function () {
+        let id = $(this).attr("data-id");
+        if (confirm("Yakin Akan Menghapus ?")) {
+            deleteData(id);
+        }
+    });
+
+    $("tbody").on("click",".btn-ubah", function () {
+        let id = $(this).attr("data-id");
+        $("#title").html("Ubah Data");
+        selectUpdate(id);
+    });
+
+    function selectUpdate(id) {
+        let idpelanggan = {
+            idpelanggan : id
+        }
+
+        $.ajax({
+            type: "pos",
+            url: "php/selectupdate.php",
+            caches: false,
+            data: JSON.stringify(idpelanggan),
+            // dataType: "dataType",
+            success: function (response) {
+                let data = JSON.parse(response)
+                
+                $("#id").val(data.idpelanggan);
+                $("#pelanggan").val(data.pelanggan);
+                $("#alamat").val(data.alamat);
+                $("#telp").val(data.telp);
+            }
+        });
+    }
+
+    function selectData() {
+        $.ajax({
+            type: "get",
+            url: "php/select.php",
+            dataType: "json",
+            success: function (response) {
+                let out = "";
+                let No = 1;
+                $.each(response, function (key, val) { 
+                    out += `<tr>
+                        <td>${No++}</td>
+                        <td>${val.pelanggan}</td>
+                        <td>${val.alamat}</td>
+                        <td>${val.telp}</td>
+                        <td><button type="button" class="btn btn-primary btn-del" data-id=${val.idpelanggan}>Hapus</button></td>
+                        <td><button type="button" class="btn btn-info btn-ubah" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id=${val.idpelanggan}>Ubah</button></td>
+                    </tr>`;
+                });
+                $("#isidata").html(out);
+            }
+        });
+    }
+
+    function insertData() {
+        let dataPelanggan = {
+            pelanggan : pelanggan,
+            alamat : alamat,
+            telp : telp
+        }
+
+        $.ajax({
+            type: "post",
+            url: "php/insert.php",
+            caches: false,
+            data: JSON.stringify(dataPelanggan),
+            // dataType: "dataType",
+            success: function (response) {
+                let out = `<p>${response}</p>`
+                $("#msg").html(out);
+            }
+        });
+
+        selectData();
+    }
+
+    function deleteData(id) {
+        let idpelanggan = {
+            idpelanggan : id
+        }
+
+        $.ajax({
+            type: "pos",
+            url: "php/delete.php",
+            caches: false,
+            data: JSON.stringify(idpelanggan),
+            // dataType: "dataType",
+            success: function (response) {
+                let out = `<p>${response}</p>`
+                $("#msg").html(out);
+            }
+        });
+
+        selectData();
+    }
+
+    function updateData() {
+        let dataPelanggan = {
+            idpelanggan : id,
+            pelanggan : pelanggan,
+            alamat : alamat,
+            telp : telp
+        }
+
+        $.ajax({
+            type: "post",
+            url: "php/update.php",
+            caches: false,
+            data: JSON.stringify(dataPelanggan),
+            // dataType: "dataType",
+            success: function (response) {
+                let out = `<p>${response}</p>`
+                $("#msg").html(out);
+            }
+        });
+
+        selectData();
+    }
+
+    selectData();
+
+});
